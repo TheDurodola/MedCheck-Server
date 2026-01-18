ebac4ce803dbe6fea559299544e0b5bb92c6dcf7
Durodola Abolaji <bolajidurodola@gmail.com>
2026-01-18 04:41:23 +0100

    feat: rename the dockerfile

--- src/test/java/com/yrsd/medcheck/controllers/AuthControllerTest.java
+++ src/test/java/com/yrsd/medcheck/controllers/AuthControllerTest.java
@@ -1,5 +1,6 @@
 package com.yrsd.medcheck.controllers;
 
+import com.yrsd.medcheck.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,6 +15,6 @@ import tools.jackson.databind.ObjectMapper;
 //    private ObjectMapper objectMapper; // Tool to convert Objects <-> JSON
 //
 //    @MockBean
-//    private AuthServices authServices;
+//    private AuthService authService;
 //
-//}
+//
